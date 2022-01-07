73afaefada19cfdff114b0ad46404dfd0a51291d
KnDjoshua <89912504+kdjoshua@users.noreply.github.com>
2022-01-07 15:12:25 +0300

    Add files via upload

---
+++ getstarted.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>getstarted</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/icon.ico" rel="icon">
+  <link href="assets/img/icon.ico" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+  
+      <!-- Uncomment below if you prefer to use an image logo -->
+       <a href="index.php" class="logo"><img src="assets/img/icon.ico" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li ><a href="getstarted.php">GET STARTED</a></li>
+         
+      </nav><!-- .nav-menu -->
+
+      <a href="index.php" class="get-started-btn btn-get-started scrollto">HOME</a>
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+      
+          <ol>
+
+            <li><a class="btn-get-started" style="color: dodgerblue;" href="index.php">Home</a></li>
+            <li>get started</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section class="container inner-page">
+      <div class="">
+      <div class="section-title">
+        <h3 style="font-family:Raleway, sans-serif;font-size: 20px ! important;">PLEASE FILL OUT THIS FORM AND WE WILL GET BACK TO YOU WITHIN TWO HOURS OF WORKING TIME.</h3>
+      </div>
+    <div class="container">
+        <form action="https://formsubmit.co/isaac.ssekisambu@thezenjleadership.com"  method="POST" role="form" class="php-email-form">
+            <div class="form-row" >
+               <div class="col-lg-6 mt-4 mt-lg-0">
+                <label>Name<span style="color:red;">*</span></label>
+                <input style="width: 100%;" type="text" name="NAME" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" required />
+                <div class="validate"></div>
+              </div>
+              <div class="col-md-6 form-group">
+                <label>Email<span style="color:red;">*</span></label>
+                <input style="width: 100%;" type="email" class="form-control" name="ENAIL" id="email" data-rule="email" data-msg="Please enter a valid email" required />
+                <div class="validate"></div>
+              </div>
+
+ <div class="col-md-6 form-group">
+                <label>Whatsapp<span style="color:red;">*</span></label>
+                <input style="width: 100%;" type="text" class="form-control" name="ENAIL" id="email" data-rule="email" data-msg="Please enter a valid email" required/>
+                <div class="validate"></div>
+              </div>
+               
+              <div class="col-lg-6 mt-4 mt-lg-0">
+            <div class="form-group">
+              <label>Package<span style="color:red;">*</span></label>
+              <select name="PACKAGE" style="width: 100%;" class="form-control form-select wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false">
+  <option value=""></option>                
+  <option value="Training Package.">Training Package.</option>
+  <option value="Coaching Package.">Coaching Package.</option>
+  <option value="Mentorship Package.">Mentorship Package.</option>
+  <option value="Start Up Package.">Start Up Package.</option>
+<div class="validate"></div></div>
+            </div></select>
+            <div class="form"></div></div></div>
+            <br>
+            <button style="background-color: dodgerblue;border: none;border-radius: 5px;height: 50px;width: 30%;" type="submit">Send</button></div>
+
+          </section>
+
+
+
+ 
+          <?php include("footer.html"); ?>
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+ 
